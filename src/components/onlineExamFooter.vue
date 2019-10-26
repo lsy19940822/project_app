fab839f3cf5f59c0856a4b2713b5837bda980c5e
liangkangsheng <1291023017@qq.com>
2019-10-26 19:21:52 +0800

    footer

--- src/components/onlineExamFooter.vue
+++ src/components/onlineExamFooter.vue
@@ -3,8 +3,8 @@
 	<!--在线考试底部-->
 	<div class="exam-footer">
 		<van-row>
-			<van-col span="8">
-				<div class="icon-box">
+			<van-col span="8" >
+				<div class="icon-box" @click="send()">
 					<van-icon name="newspaper-o" />
 					<p class="f-text">答题卡</p>
 				</div>
@@ -13,12 +13,12 @@
 				<div class="icon-box">
 					<van-icon name="underway-o" style="font-size: 26px;margin-top: -5px;" />
 					<div class="f-text">
-						<van-count-down millisecond :time="time" format="HH:mm:ss" ref= 'timesVal' />
+						<van-count-down millisecond :time="time" format="mm:ss" />
 					</div>
 				</div>
 			</van-col>
 			<van-col span="8">
-				<div class="icon-box">
+				<div class="icon-box" @click="examItem()">
 					<van-icon name="todo-list-o" />
 					<p  class="f-text">交卷</p>
 				</div>
@@ -38,9 +38,21 @@
 		},
 		data() {
 			return {
-				time:30*60*60*1000
+				time:60*60*60*12.5,
+				show:true,
+			}
+		},
+		methods:{
+			send(){
+				let showButton=this.show;
+				this.$emit("list",showButton);
+			},
+			examItem(){
+				let showButton=this.show;
+				this.$emit("examItem",showButton);
 			}
 		}
+		
 	}
 </script>
 
@@ -58,10 +70,11 @@
 		right:0;
 		bottom:0;
 		width: 100%;
-		height: 58px;
+		/* height: 58px; */
 		text-align: center;
 		background: url(../assets/images/exam/bg_exam_footer.png) no-repeat;
-		background-size: contain;
+		background-size: 100% 100%;
+
 	}
 	.f-text{
 		color:#888888;
