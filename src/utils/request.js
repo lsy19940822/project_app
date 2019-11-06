51fe5c59b34fc49bcbbb74c4e7b8eba1c8402b3b
a2680880012 <2680880012@qq.com>
2019-11-06 21:54:06 +0800

    -

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
-import {Toast} from 'vant';
+import { Toast } from 'vant';
 // 引用vuex 来判断 token 
 // import store from '../store/store.js'
 // 创建 axios 实例
 export const Axios = axios.create({
-  baseURL: 'http://47.95.118.243/API/WebAPIDataAudit/', //测试服
-  baseURL: 'http://47.95.118.243/API/WebAPIDataAudit/', //正式服
-  timeout: 5000,  //请求接口的时间 毫秒
+	baseURL: 'http://47.95.118.243/API/WebAPIDataAudit/', //测试服
+	baseURL: 'http://47.95.118.243/API/WebAPIDataAudit/', //正式服
+	timeout: 5000, //请求接口的时间 毫秒
 })
 // 添加请求拦截器
 Axios.interceptors.request.use(function(config) {
@@ -22,18 +22,17 @@ Axios.interceptors.request.use(function(config) {
 });
 // 添加响应拦截器
 Axios.interceptors.response.use(function(response) {
-	// console.log(response.data.code)
-		if (response.status === 200) {            
-            return Promise.resolve(response);        
-        } else {            
-            return Promise.reject(response);        
-        }   
+	console.log(response)
+	if(response.status === 200) {
+		return Promise.resolve(response);
+	} else {
+		return Promise.reject(response);
+	}
 	// 对响应数据做点什么
 	return response;
 }, function(error) {
 	// 对响应错误做点什么
 	return Promise.reject(error);
 });
-
 
 export default Axios
--- src/views/exam/manbT.vue
+++ src/views/exam/manbT.vue
@@ -1,14 +1,25 @@
 <template>
 	<div id="examrecord">
-		<vant-header :leftArrow="true" :titleType="1" :title="questionText" :rightType="2">
-			
-		</vant-header>
+		<vant-header :leftArrow="true" :titleType="1" :title="questionText" :rightType="2"></vant-header>
 		<ul id="Bt_list">
-			<li><img src="../../assets/exam_man@2x (3).png" alt=""><div>光线充足<br>正对手机</div></li>
-			<li><img src="../../assets/exam_man@2x (2).png" alt=""><div>不要在灰暗<br>或逆光下扫描</div></li>
-			<li><img src="../../assets/exam_man@2x (1).png" alt=""><div>不要斜视<br>手机</div></li>
+			<li><img src="../../assets/exam_man@2x (3).png" alt="">
+				<div>光线充足<br>正对手机</div>
+			</li>
+			<li><img src="../../assets/exam_man@2x (2).png" alt="">
+				<div>不要在灰暗<br>或逆光下扫描</div>
+			</li>
+			<li><img src="../../assets/exam_man@2x (1).png" alt="">
+				<div>不要斜视<br>手机</div>
+			</li>
 		</ul>
-		<div class="button">开始验证</div>
+		<div class="scanning-box" v-show="scanImg"></div>
+		<div class="uploader-img-box" v-show="scanImg">
+			<span class="line"></span>
+			<van-image :src="uploaderImg" class="uploader-img" />
+		</div>
+		<van-uploader :after-read="afterRead" class="upload-btn" ref="uploadBtn">
+			<van-button type="primary" :square="true" size="large" style="font-size: 16px;">开始验证</van-button>
+		</van-uploader>
 	</div>
 </template>
 
@@ -16,8 +27,20 @@
 	import vantHeader from '@/components/header.vue'
 	import * as ajax from '@/utils/api'
 	import Vue from 'vue';
-	import { Cell} from 'vant';
-	
+	import { Cell } from 'vant';
+	import { Uploader } from 'vant';
+	import { Button } from 'vant';
+	import { Image } from 'vant';
+	import { Toast } from 'vant';
+
+	Vue.use(Toast);
+
+	Vue.use(Image);
+
+	Vue.use(Button);
+
+	Vue.use(Uploader);
+
 	Vue.use(Cell)
 	export default {
 		components: {
@@ -25,57 +48,72 @@
 		},
 		data() {
 			return {
-				questionText:"人脸识别"
+				questionText: "人脸识别",
+				uploaderImg: "",
+				scanImg: false
 			}
 		},
-		mounted() {
-		},
 		created() {
-			
+
+		},
+		mounted() {
+			// dom渲染完后将UI上传组件的input添加属性，使之只可以拍照上传
+			this.$refs.uploadBtn.$el.getElementsByTagName("input")[0].setAttribute("capture", "camera");
 		},
 		methods: {
-			
-			
+			afterRead(file) {
+				var _this = this;
+				this.scanImg = true;
+				console.log(file)
+				this.uploaderImg = file.content;
+				setTimeout(function() {
+					ajax.postW('faceRecognition/recognizeFace', {
+						recognizeFaceImageUrl: _this.uploaderImg,
+						groupName: '2标'
+					}).then(res => {
+						console.log(res)
+						_this.scanImg = false;
+						if(res.status == 200 && res.data.success) {
+							Toast('身份信息展示开发中...');
+						} else {
+							Toast(res.data.msg);
+						}
+					});
+				}, 1500)
+			}
 		}
 	}
 </script>
 
 <style scoped>
-	#examrecord{
-		/* padding-top: 46px; */
+	#examrecord {
+		padding-top: 46px;
 		background: #fff;
 		position: fixed;
-		top: 46px;
+		top: 0;
 		width: 100%;
 		left: 0;
 		right: 0;
 		bottom: 0;
-		
+		overflow: auto;
 	}
-	.button{
-		width: 90%;
-		height:46px;
-		margin: 0 auto;
-		background: #00A0E9;
-		color: #fff;
-		line-height: 46px;
-		border-radius: 2px;
-		text-align: center;
-	}
-	#Bt_list{
+	
+	#Bt_list {
 		width: 90%;
 		height: auto;
 		overflow: hidden;
 		margin: 0 auto;
 	}
-	li{
+	
+	li {
 		width: 100%;
 		height: auto;
 		padding: 20px 0;
 		overflow: hidden;
 		border-bottom: 1px solid #ddd;
 	}
-	li img{
+	
+	li img {
 		width: 108px;
 		height: 108px;
 		border: 1px dashed #ddd;
@@ -83,8 +121,171 @@
 		display: block;
 		margin-left: 50px;
 	}
-	li div{
+	
+	li div {
 		margin: 24px 16px;
 		float: left;
+	}
+	
+	input.file {
+		position: relative;
+		-moz-opacity: 0;
+		filter: alpha(opacity: 0);
+		opacity: 0;
+		z-index: 2;
+	}
+	
+	.wrapper {
+		color: #fff;
+		background-color: #31b0d5;
+		border-color: #269abc;
+		margin-top: 5px;
+		margin-bottom: 5px;
+		display: inline-block;
+		padding: 6px 12px;
+		margin-bottom: 0;
+		font-size: 14px;
+		font-weight: 400;
+		line-height: 1.42857143;
+		text-align: center;
+		white-space: nowrap;
+		vertical-align: middle;
+		-ms-touch-action: manipulation;
+		touch-action: manipulation;
+		cursor: pointer;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+		background-image: none;
+		border: 1px solid transparent;
+		border-radius: 4px;
+	}
+	
+	.upload-btn {
+		margin-bottom: 10px;
+	}
+	
+	.scanning-box {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background: rgba(0, 0, 0, .3);
+		z-index: 49;
+	}
+	
+	.uploader-img-box {
+		position: fixed;
+		top: 50%;
+		left: 20%;
+		z-index: 50;
+		width: 60%;
+		height: auto;
+		transform: translateY(-50%);
+		-webkit-transform: translateY(-50%);
+		-moz-transform: translateY(-50%);
+		-ms-transform: translateY(-50%);
+		-o-transform: translateY(-50%);
+	}
+	
+	.uploader-img-box:before,
+	.uploader-img-box:after,
+	.uploader-img:before,
+	.uploader-img:after {
+		content: '';
+		display: inline-block;
+		position: absolute;
+		width: 30px;
+		height: 30px;
+		border: 5px solid #fff;
+	}
+	
+	.uploader-img-box:before {
+		top: -10px;
+		left: -10px;
+		border-right: none;
+		border-bottom: none;
+	}
+	
+	.uploader-img-box:after {
+		right: -10px;
+		top: -10px;
+		border-left: none;
+		border-bottom: none;
+	}
+	
+	.uploader-img:before {
+		bottom: -10px;
+		left: -10px;
+		border-top: none;
+		border-right: none;
+	}
+	
+	.uploader-img:after {
+		right: -10px;
+		bottom: -10px;
+		border-left: none;
+		border-top: none;
+	}
+	
+	.line {
+		display: inline-block;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 3px;
+		background: #666;
+		border-radius: 50%;
+		z-index: 51;
+		animation: moving 1s linear infinite;
+		-webkit-animation: moving 1s linear infinite;
+		-moz-animation: moving 1s linear infinite;
+		-ms-animation: moving 1s linear infinite;
+		-o-animation: moving 1s linear infinite;
+	}
+	
+	@keyframes moving {
+		0% {
+			top: 0;
+		}
+		100% {
+			top: 100%;
+		}
+	}
+	
+	@-webkit-keyframes moving {
+		0% {
+			top: 0;
+		}
+		100% {
+			top: 100%;
+		}
+	}
+	
+	@-moz-keyframes moving {
+		0% {
+			top: 0;
+		}
+		100% {
+			top: 100%;
+		}
+	}
+	
+	@-ms-keyframes moving {
+		0% {
+			top: 0;
+		}
+		100% {
+			top: 100%;
+		}
+	}
+	
+	.uploader-img {
+		display: inline-block;
+		width: 100%;
+		height: auto;
 	}
 </style>
